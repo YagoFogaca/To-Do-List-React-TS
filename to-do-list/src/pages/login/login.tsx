bfbfd68aa64f82462e94560b17bc669613508a55
Yago Fogaca <yagofogaca20@gmail.com>
2022-10-05 15:16:28 -0300

    fix: rename style of style-login

--- to-do-list/src/pages/login/login.tsx
+++ to-do-list/src/pages/login/login.tsx
@@ -1,5 +1,5 @@
 import { FormLogin } from '../../components/form-login/form-login';
-import { SectionLogin } from './style';
+import { SectionLogin } from './style-login';
 
 export function Login() {
   return (
