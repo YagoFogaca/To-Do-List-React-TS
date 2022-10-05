f6185c5e7d6fb1ec09c29e557be154ed2b07abdb
Yago Fogaca <yagofogaca20@gmail.com>
2022-10-05 14:58:28 -0300

    feat: font implementation

--- to-do-list/src/styles/global.ts
+++ to-do-list/src/styles/global.ts
@@ -1,11 +1,14 @@
 import { createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle`
+@import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400&display=swap');
     *{
         margin: 0;
         padding: 0;
         outline: 0;
         box-sizing: border-box;
+        font-size: 10px;
+        font-family: 'Open Sans', sans-serif;
     }
     html, body, #root {
         height: 100%;
