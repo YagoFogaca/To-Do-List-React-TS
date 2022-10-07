2daad07a2c1ed2754c124463489dd78fcc5ed247
Yago Fogaca <yagofogaca20@gmail.com>
2022-10-06 21:41:17 -0300

    feat: react-router implementation

--- to-do-list/src/App.tsx
+++ to-do-list/src/App.tsx
@@ -1,4 +1,12 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Login } from './pages/login/login';
+
 export function App() {
-  return <Login />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
